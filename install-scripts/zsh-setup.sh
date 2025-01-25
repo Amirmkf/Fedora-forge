f284923fbb47d479e0f8a032d31bfbdd37d25191
Amir Mahdi Kiani fard <98949100+Amirmkf@users.noreply.github.com>
2025-01-25 18:44:27 +0330

    Update zsh-setup.sh
    some bugs of zsh fixed

--- install-scripts/zsh-setup.sh
+++ install-scripts/zsh-setup.sh
@@ -6,15 +6,25 @@
 GREEN="\033[0;32m"
 RESET="\033[0m"
 
+# Install Zsh and required utilities
+echo -e "${GREEN}Installing Zsh and required utilities...${RESET}"
 sudo dnf install -y zsh util-linux-user
 
 # Set Zsh as the default shell
-chsh -s $(which zsh)
+if [[ "$SHELL" != "$(which zsh)" ]]; then
+  echo -e "${GREEN}Setting Zsh as the default shell...${RESET}"
+  chsh -s "$(which zsh)"
+else
+  echo "Zsh is already the default shell."
+fi
 
 # Install Oh My Zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo -e "${GREEN}Installing Oh My Zsh...${RESET}"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || {
+    echo "Failed to install Oh My Zsh. Exiting."
+    exit 1
+  }
 else
   echo "Oh My Zsh is already installed."
 fi
@@ -23,27 +33,19 @@ fi
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 echo -e "${GREEN}Installing Zsh plugins...${RESET}"
 
-# Essential plugins
 declare -A plugins=(
   ["zsh-autosuggestions"]="https://github.com/zsh-users/zsh-autosuggestions"
   ["zsh-syntax-highlighting"]="https://github.com/zsh-users/zsh-syntax-highlighting"
   ["zsh-completions"]="https://github.com/zsh-users/zsh-completions"
-)
-
-# Developer-focused plugins
-plugins+=(
-#  ["zsh-docker"]="https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/docker"
-  ["zsh-git"]="https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git"
-  ["zsh-npm"]="https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/npm"
-  ["zsh-yarn"]="https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/yarn"
   ["zsh-nvm"]="https://github.com/lukechilds/zsh-nvm"
 )
 
-# Install plugins
 for plugin in "${!plugins[@]}"; do
   if [ ! -d "${ZSH_CUSTOM}/plugins/$plugin" ]; then
     echo -e "${GREEN}Installing $plugin...${RESET}"
-    git clone "${plugins[$plugin]}" "${ZSH_CUSTOM}/plugins/$plugin"
+    git clone "${plugins[$plugin]}" "${ZSH_CUSTOM}/plugins/$plugin" || {
+      echo "Failed to install $plugin."
+    }
   else
     echo "$plugin is already installed."
   fi
@@ -52,7 +54,11 @@ done
 # Install Powerlevel10k theme
 if [ ! -d "${ZSH_CUSTOM}/themes/powerlevel10k" ]; then
   echo -e "${GREEN}Installing Powerlevel10k theme...${RESET}"
-  git clone https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}/themes/powerlevel10k"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}/themes/powerlevel10k" || {
+    echo "Failed to install Powerlevel10k theme."
+  }
+else
+  echo "Powerlevel10k theme is already installed."
 fi
 
 # Configure .zshrc
@@ -60,6 +66,7 @@ echo -e "${GREEN}Configuring .zshrc...${RESET}"
 
 # Ensure ~/.zshrc exists
 if [ ! -f ~/.zshrc ]; then
+  echo "Creating .zshrc..."
   touch ~/.zshrc
 fi
 
@@ -75,34 +82,31 @@ NEW_PLUGINS="plugins=(
     nvm
 )"
 NEW_THEME='ZSH_THEME="powerlevel10k/powerlevel10k"'
+SOURCE_LINE="source \$ZSH/oh-my-zsh.sh"
 
 # Update the plugins line
 if grep -q "^plugins=(" ~/.zshrc; then
-  # Replace the existing plugins block in place
-  sed -i "/^plugins=(/,/^)/{
-    r /dev/stdin
-    d
-  }" ~/.zshrc <<< "$NEW_PLUGINS"
+  sed -i "/^plugins=(/,/^)/c\\$NEW_PLUGINS" ~/.zshrc
 else
-  # Add the plugins line if it doesn't exist
   echo "$NEW_PLUGINS" >> ~/.zshrc
 fi
 
-# Uncomment and update the ZSH_THEME line
-if grep -q "^#\s*ZSH_THEME=" ~/.zshrc; then
-  # Uncomment the line if it's commented out (use | as the delimiter)
-  sed -i "s|^#\s*ZSH_THEME=.*|$NEW_THEME|" ~/.zshrc
-elif grep -q "^ZSH_THEME=" ~/.zshrc; then
-  # Replace the existing ZSH_THEME line (use | as the delimiter)
+# Add the source line after plugins if not present
+if ! grep -q "^source \$ZSH/oh-my-zsh.sh" ~/.zshrc; then
+  sed -i "/^plugins=(/,/^)/a $SOURCE_LINE" ~/.zshrc
+fi
+
+# Update the theme line
+if grep -q "^ZSH_THEME=" ~/.zshrc; then
   sed -i "s|^ZSH_THEME=.*|$NEW_THEME|" ~/.zshrc
 else
-  # Add the ZSH_THEME line if it doesn't exist
   echo "$NEW_THEME" >> ~/.zshrc
 fi
 
-# Apply changes
+# Source the updated .zshrc
 echo -e "${GREEN}Applying changes...${RESET}"
 zsh -c "source ~/.zshrc"
 
-
+# Clear the screen
 clear
+echo -e "${GREEN}Setup complete! Restart your terminal or type 'zsh' to use your new configuration.${RESET}"
